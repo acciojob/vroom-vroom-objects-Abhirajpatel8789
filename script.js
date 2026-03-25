d1e6191566867b46b8add751e6d0da76ecd74315
Abhirajpatel8789 <122907724+Abhirajpatel8789@users.noreply.github.com>
2026-03-25 11:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function Car(make, model) {
 Car.prototype.getMakeModel = function() {
     return this.make + " " + this.model;
 };
+window.Car = Car;
 
 function SportsCar(make, model, topSpeed) {
-	car.call(this,make,model)
+	Car.call(this,make,model)
 	this.topSpeed = topSpeed
 }
 SportsCar.prototype = Object.create(Car.prototype);
@@ -21,5 +22,9 @@ SportsCar.prototype.getTopSpeed = function() {
 	return this.topSpeed;
 };
 // Do not change the code below
-window.Car = Car;
 window.SportsCar = SportsCar;
+
+let car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed()); 
+
